0922878572545e7326e4cdd0fdf4842b5fc723bd
Saidulloh <ssavutokhunov@gmail.com>
2022-12-25 13:14:17 +0600

    edit views

--- apps/user/views.py
+++ apps/user/views.py
@@ -33,7 +33,16 @@ class UserUpdateDestroyAPIView(GenericViewSet,
         return self.check(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
-        return self.check(request, *args, **kwargs)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 class UserAPIViewSet(GenericViewSet,
